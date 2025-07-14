3cbddf3a463feffcc20797fc608d99a61b5b21ee
jeseed <jesse@cedar.law>
2025-07-14 14:32:26 -0700

    chore: remove unneeded tiptap type changes

--- packages/cedar/src/types/tiptap-react.d.ts
+++
@@ -1,9 +0,0 @@
-import type { EditorContentProps } from '@tiptap/react';
-import type React from 'react';
-
-declare module '@tiptap/react' {
-	/**
-	 * Override EditorContent to be recognized as a valid React component
-	 */
-	export const EditorContent: React.ComponentType<EditorContentProps>;
-}
